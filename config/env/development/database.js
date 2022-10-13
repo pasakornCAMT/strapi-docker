20f25246507e27aa9ca0e34c612b93eebd82e376
pasakornCRMC <pasakorn@crmc.consulting>
2022-10-13 20:24:41 +0700

    installed deployify

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -1,13 +1,19 @@
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+
 module.exports =  ({ env }) => ({
-	connection: {
-		client: 'postgres',
-		connection: {
-		host: env('DATABASE_HOST', 'localhost'),
-			port: env.int('DATABASE_PORT', 5432),
-			database: env('DATABASE_NAME', 'strapi'),
-			user: env('DATABASE_USERNAME', 'strapi'),
-			password: env('DATABASE_PASSWORD', '1q2w3e4r5t'),
-			ssl: env.bool('DATABASE_SSL', false)
-		}
-	}
+connection: {
+    client: "postgres",
+    connection: {
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+    debug: false,
+  },
 });
