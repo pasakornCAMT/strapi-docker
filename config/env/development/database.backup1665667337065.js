3c809301c0998f1ffeb22b0026a6992e0ac68bce
pasakornCRMC <pasakorn@crmc.consulting>
2022-10-15 16:32:00 +0700

    changed db host

--- config/env/development/database.backup1665667337065.js
+++ config/env/development/database.backup1665667337065.js
@@ -2,7 +2,7 @@ module.exports =  ({ env }) => ({
 	connection: {
 		client: 'postgres',
 		connection: {
-		host: env('DATABASE_HOST', 'localhost'),
+		host: env('DATABASE_HOST', 'strapi-dockerDB'),
 			port: env.int('DATABASE_PORT', 5432),
 			database: env('DATABASE_NAME', 'strapi'),
 			user: env('DATABASE_USERNAME', 'strapi'),
--- config/env/development/database.js
+++ config/env/development/database.js
@@ -2,7 +2,7 @@ module.exports =  ({ env }) => ({
 	connection: {
 		client: env("DATABASE_CLIENT", "postgres"),
 		connection: {
-		host: env('DATABASE_HOST', 'localhost'),
+		host: env('DATABASE_HOST', 'strapi-dockerDB'),
 			port: env.int('DATABASE_PORT', 5432),
 			database: env('DATABASE_NAME', 'strapi'),
 			user: env('DATABASE_USERNAME', 'strapi'),
