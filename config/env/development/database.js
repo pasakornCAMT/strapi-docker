391ec94541bfb4245049d0f6f7165f9f0d619dc0
pasakornCRMC <pasakorn@crmc.consulting>
2022-10-15 15:09:11 +0700

    updated

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -1,8 +1,8 @@
 module.exports =  ({ env }) => ({
 	connection: {
-		client: 'postgres',
+		client: env("DATABASE_CLIENT", "postgres"),
 		connection: {
-		host: env('DATABASE_HOST', 'localhost'),
+		host: env('DATABASE_HOST', '127.0.0.1'),
 			port: env.int('DATABASE_PORT', 5432),
 			database: env('DATABASE_NAME', 'strapi'),
 			user: env('DATABASE_USERNAME', 'strapi'),
